82d91966704b368b3eadc447c1e5f6d9fb7220df
Mvfm <mvfm.filho@hotmail.com>
2025-11-17 15:32:51 -0300

    Primeira versão do método 'bwImage' funcionando. Vou testar mais com outras extensões mais usadas.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 // Estado inicial do projeto. Aprendendo a como incorporar arquivos da biblioteca stb_image para minha conveniência.
+// Por agora, estou testando apenas o método de imagens grayscale.
 // [mvfm]
 
-// Criado : 05/11/2025  ||  Última modificação: 05/11/2025
+// Criado : 05/11/2025  ||  Última modificação: 17/11/2025
 
 #include <iostream>
 #include <fstream>
@@ -19,8 +20,7 @@ int lerArquivo(std::string caminhoArquivo);
 int bwImagem(std::string caminhoArquivo);
 
 int main(){
-    std::string caminhoArquivo = "img/texto.txt";
-    lerArquivo(caminhoArquivo);
+    bwImagem("img/Puppet_Tails_Is_Shocked.png");
     return 0;
 }
 
@@ -51,7 +51,9 @@ int lerArquivo(std::string caminhoArquivo){
 
 int bwImagem(std::string caminhoArquivo){
     int largura, altura, canais;
+
     // Fazendo a conversão de String para uma 'C-Style' string.
+    // Do jeito que estou lendo, utilizando o stbi, consigo carregar qualquer formato comum.
     unsigned char* img = stbi_load(caminhoArquivo.c_str(), &largura, &altura, &canais, 0);
 
     // Caso null, basicamente.
@@ -60,11 +62,54 @@ int bwImagem(std::string caminhoArquivo){
         return 1;
     }
 
-    typedef unsigned char uchar;
-    std::vector<uchar> dadosPixel(altura * largura);
-    char saida[] = "saida.png";
-    
-    for(int i = 0; i < altura * largura; i++){
+    // Pra ter certeza que só retorna 1 canal, Grayscale
+    std::vector<unsigned char> imgBW(largura * altura);
 
+    // Bem auto-explicativo.
+    bool temRGB = (canais == 3 || canais == 4);
+
+    for(int y = 0; y < altura; y++){
+        for(int x = 0; x < largura; x++){
+
+            int idxOrigem = (y * largura + x) * canais;
+            unsigned char r, g, b;
+
+            if(temRGB){
+                r = img[idxOrigem + 0];
+                g = img[idxOrigem + 1];
+                b = img[idxOrigem + 2];
+            } else {
+                r = g = b = img[y * largura + x];
+            }
+
+            // Convertendo finalmente para cinza.
+            unsigned char cinza = (unsigned char)(0.299 * r + 0.587 * g + 0.114 * b);
+
+            imgBW[y * largura + x] = cinza;
+
+        }
     }
+
+    // Fazendo parte de saída
+    // Sempre salvando como png por enquanto.
+    const char* saida = "img/saida_bw.png";
+
+    int sucesso = stbi_write_png(
+        saida,
+        largura,
+        altura,
+        1, // 1 canal.
+        imgBW.data(),
+        largura
+    );
+
+    if(!sucesso){
+        std::cerr << "Falha ao salvar a imagem resultante!" << std::endl;
+        stbi_image_free(img);
+        return 1;
+    }
+
+    std::cout << "Imagem resultante salva como : "  << saida << std::endl;
+    stbi_image_free(img);
+    return 0;
 }
