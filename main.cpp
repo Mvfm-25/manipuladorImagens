f0d728b1ac6812d4155c63a0ae7f7c7830a46647
Mvfm <mvfm.filho@hotmail.com>
2025-11-18 14:30:56 -0300

    Método de inversor de cores. Novos exemplos na pasta 'img'.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Por agora, estou testando apenas o método de imagens grayscale.
 // [mvfm]
 
-// Criado : 05/11/2025  ||  Última modificação: 17/11/2025
+// Criado : 05/11/2025  ||  Última modificação: 18/11/2025
 
 #include <iostream>
 #include <fstream>
@@ -18,9 +18,11 @@
 // Protótipos
 int lerArquivo(std::string caminhoArquivo);
 int bwImagem(std::string caminhoArquivo);
+int revImagem(std::string caminhoArquivo);
 
 int main(){
     bwImagem("img/Puppet_Tails_Is_Shocked.png");
+    revImagem("img/Puppet_Tails_Is_Shocked.png");
     return 0;
 }
 
@@ -52,8 +54,8 @@ int lerArquivo(std::string caminhoArquivo){
 int bwImagem(std::string caminhoArquivo){
     int largura, altura, canais;
 
-    // Fazendo a conversão de String para uma 'C-Style' string.
     // Do jeito que estou lendo, utilizando o stbi, consigo carregar qualquer formato comum.
+    // Adendo : Testando ontem, carregando uma imagem .webp parece estar dando erro, mas não tenho certeza o pq.
     unsigned char* img = stbi_load(caminhoArquivo.c_str(), &largura, &altura, &canais, 0);
 
     // Caso null, basicamente.
@@ -112,4 +114,70 @@ int bwImagem(std::string caminhoArquivo){
     std::cout << "Imagem resultante salva como : "  << saida << std::endl;
     stbi_image_free(img);
     return 0;
+}
+
+// Método para inverter as cores de uma imagem.
+int revImagem(std::string caminhoArquivo){
+
+    std::cout << "Iniciando método para inverter as cores da imagem...";
+
+    // Por enquanto, mesma 'filosofia' que o bwImagem.
+    // Talvez seja legal separar todo esse caminho repetido em seu próprio método.
+    int largura, altura, canais;
+    unsigned char* img = stbi_load(caminhoArquivo.c_str(), &largura, &altura, &canais, 0);
+
+    // Levando em consideração os canais para o tamanho do novo arquivo.
+    // Só leva em consideração os canais só no caso do bw.
+    std::vector<unsigned char> revImagem(largura * altura * canais);
+
+    if(!img){
+        std::cout << "Falha ao carregar imagem!";
+        return 1;
+    }
+
+    for(int y = 0; y < altura; y++){
+        for(int x = 0; x < largura; x++){
+
+            int idxOrigem = (y * largura + x) * canais;
+            unsigned char r, g, b;
+
+            // Mesma coisa, agora sem condicional pois estou assumindo que toda imagem enviada por aqui tem RGB.
+            r = 255 - img[idxOrigem + 0];
+            g = 255 - img[idxOrigem + 1];
+            b = 255 - img[idxOrigem + 2];
+
+            revImagem[idxOrigem + 0] = r;
+            revImagem[idxOrigem + 1] = g;
+            revImagem[idxOrigem + 2] = b;
+
+            // Se tiver canal alpha, copie sem modificar
+            if(canais == 4){
+                revImagem[idxOrigem + 3] = img[idxOrigem + 3];
+            }
+        }
+    }
+
+    // Fazendo parte de saída
+    // Sempre salvando como png por enquanto.
+    const char* saida = "img/saida_inv.png";
+
+    int sucesso = stbi_write_png(
+        saida,
+        largura,
+        altura,
+        4, // 4 canais, assumindo alpha.
+        revImagem.data(),
+        largura * canais // De novo, considerando os canais.
+    );
+
+    if(!sucesso){
+        std::cerr << "Falha ao salvar a imagem resultante!" << std::endl;
+        stbi_image_free(img);
+        return 1;
+    }
+
+    std::cout << "Imagem resultante salva como : "  << saida << std::endl;
+    stbi_image_free(img);
+    return 0;
+
 }
